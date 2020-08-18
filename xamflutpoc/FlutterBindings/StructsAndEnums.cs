136d3107825cc0843ba86375c53e74ea312f178e
Nicolai Henriksen <nih@miracle.dk>
2020-08-18 10:30:40 +0200

    Initial commit.
    Created flutter framework, auto-generated Xamarin binding, Xamarin project. Nothing works yet.

---
+++ xamflutpoc/FlutterBindings/StructsAndEnums.cs
@@ -0,0 +1,16 @@
+using ObjCRuntime;
+
+[Native]
+public enum FlutterStandardDataType : long
+{
+	UInt8,
+	Int32,
+	Int64,
+	Float64
+}
+
+public enum FlutterPlatformViewGestureRecognizersBlockingPolicy : uint
+{
+	Eager,
+	WaitUntilTouchesEnded
+}
---
+++ xamflutpoc/xamflutpoc/ViewController.cs
@@ -0,0 +1,30 @@
+﻿using Foundation;
+using System;
+using UIKit;
+
+namespace xamflutpoc
+{
+    public partial class ViewController : UIViewController
+    {
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+
+        partial void UIButton197_TouchUpInside(UIButton sender)
+        {
+            Console.WriteLine("Tapped!");
+        }
+    }
+}
