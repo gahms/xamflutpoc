94fff215c31f9e9de001e0cf8ada124b1a3cfcfe
Martin Bæhrenz Bjerregaard <mbb@miracle.dk>
2020-09-10 14:58:00 +0200

    Add Android proof-of-concept with Flutter plugin support

---
+++ xamflutpoc.Android/xamflutpoc/MainActivity.cs
@@ -0,0 +1,40 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Support.V7.App;
+using Android.Support.V7.Widget;
+using IO.Flutter.Embedding.Android;
+using Xamarin.Essentials;
+
+namespace xamflutpoc
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            var toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+
+            var fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
+            fab.Click += (_, __) => LaunchFlutterActivity();
+        }
+
+        private void LaunchFlutterActivity()
+        {
+            var intent = FlutterActivity.WithNewEngine().Build(this);
+            StartActivity(intent);
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Permission[] grantResults)
+        {
+            Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ xamflutpoc.Android/xamflutpoc/MainApplication.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Android.App;
+using Android.Runtime;
+using IO.Flutter.Embedding.Engine;
+using IO.Flutter.Embedding.Engine.Dart;
+using IO.Flutter.Embedding.Engine.Plugins;
+using IO.Flutter.Plugin.Common;
+using IO.Flutter.Plugins;
+using Java.Interop;
+using Xamarin.Essentials;
+
+namespace xamflutpoc
+{
+    [Application]
+    public class MainApplication : Application
+    {
+        public const string FlutterEngineId = "MainFlutterEngine";
+
+        public MainApplication(IntPtr javaReference, JniHandleOwnership transfer)
+            : base(javaReference, transfer)
+        {
+        }
+
+        public override void OnCreate()
+        {
+            Platform.Init(this);
+            base.OnCreate();
+        }
+    }
+}
