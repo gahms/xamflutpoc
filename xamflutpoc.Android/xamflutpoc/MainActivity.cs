445cdff597df73b470692a16928891eefbbc9b3d
Martin Bæhrenz Bjerregaard <mbb@miracle.dk>
2020-09-14 10:41:19 +0200

    Add FlutterFragment on screen

--- xamflutpoc.Android/xamflutpoc/MainActivity.cs
+++ xamflutpoc.Android/xamflutpoc/MainActivity.cs
@@ -2,17 +2,19 @@
 using Android.Content.PM;
 using Android.OS;
 using Android.Runtime;
-using Android.Support.Design.Widget;
-using Android.Support.V7.App;
 using Android.Widget;
+using Google.Android.Material.FloatingActionButton;
 using IO.Flutter.Embedding.Android;
+using IO.Flutter.Embedding.Engine;
+using IO.Flutter.Embedding.Engine.Dart;
+using IO.Flutter.Plugins;
 using Xamarin.Essentials;
-using Toolbar = Android.Support.V7.Widget.Toolbar;
+using Toolbar = AndroidX.AppCompat.Widget.Toolbar;
 
 namespace xamflutpoc
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
-    public class MainActivity : AppCompatActivity
+    public class MainActivity : AndroidX.AppCompat.App.AppCompatActivity
     {
         private const string FlutterFragmentTag = "flutter_fragment";
 
@@ -38,23 +40,56 @@ namespace xamflutpoc
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
 
+        private void EnsureFlutterEngineCreated(string engineId)
+        {
+            if (!FlutterEngineCache.Instance.Contains(engineId))
+            {
+                var flutterEngine = new FlutterEngine(this);
+                GeneratedPluginRegistrant.RegisterWith(flutterEngine);
+
+                flutterEngine.DartExecutor.ExecuteDartEntrypoint(
+                    DartExecutor.DartEntrypoint.CreateDefault()
+                );
+
+                FlutterEngineCache.Instance.Put(engineId, flutterEngine);
+            }
+        }
+
         private void LaunchFlutterActivity()
         {
-            var intent = FlutterActivity.WithNewEngine().Build(this);
+            const string EngineId = "FlutterActivityEngine";
+            EnsureFlutterEngineCreated(EngineId);
+
+            var intent = FlutterActivity
+                .WithCachedEngine(EngineId)
+                .Build(this);
+
             StartActivity(intent);
         }
 
         private void ShowFlutterFragment()
         {
-            /*var container = FindViewById<FrameLayout>(Resource.Id.fragment_container);
-
             flutterFragment = SupportFragmentManager
                 .FindFragmentByTag(FlutterFragmentTag) as FlutterFragment;
 
             if (flutterFragment == null)
             {
+                const string EngineId = "FlutterFragmentEngine";
+                EnsureFlutterEngineCreated(EngineId);
 
-            }*/
+                flutterFragment = FlutterFragment
+                    .WithCachedEngine(EngineId)
+                    .Build() as FlutterFragment;
+
+                SupportFragmentManager
+                    .BeginTransaction()
+                    .Add(
+                        Resource.Id.fragment_container,
+                        flutterFragment,
+                        FlutterFragmentTag
+                    )
+                    .Commit();
+            }
         }
     }
 }
